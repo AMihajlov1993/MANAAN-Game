1e27fe95ee96cc12e01b0a71576aa503cc615ae8
soxa <emil.aleksandrov87@gmail.com>
2014-05-19 11:20:10 +0300

    update class player

--- src/Player.java
+++ src/Player.java
@@ -1,8 +1,20 @@
-
 public class Player {
+
+	public enum TicToeEnum {
+		X, O
+	}
 
 	private String Name;
 	private int Score = 0;
+	private TicToeEnum symbol;
+
+	public TicToeEnum getSymbol() {
+		return symbol;
+	}
+
+	public void setSymbol(TicToeEnum symbol) {
+		this.symbol = symbol;
+	}
 
 	public String getName() {
 		return Name;
