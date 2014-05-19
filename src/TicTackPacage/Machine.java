642772a2792269021e7a443b6723770c4ef69cbd
anichka777 <avalcheva7@gmail.com>
2014-05-19 14:28:06 +0300

    class Machine

---
+++ src/TicTackPacage/Machine.java
@@ -0,0 +1,14 @@
+package TicTackPacage;
+
+public class Machine {
+	//Paths, score to be evaluated
+	int[] firstDiagonal = new int[3];
+	int[] secondDiagonal = new int[3];
+	int[] firstLine = new int[3];
+	int[] secondLine = new int[3];
+	int[] thirdLine = new int[3];
+	int[] secondCol = new int[3];
+	int[] firstCol = new int[3];
+	int[] thirdCol = new int[3];
+
+}
