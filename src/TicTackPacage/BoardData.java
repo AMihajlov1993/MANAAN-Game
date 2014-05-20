13f9e905430d573d3d5a244a0b439ffe9a5e560e
AMihajlov1993 <skullxtreme@abv.bg>
2014-05-20 17:23:29 +0300

    Reworked BoardData

--- src/TicTackPacage/BoardData.java
+++ src/TicTackPacage/BoardData.java
@@ -5,6 +5,13 @@ public class BoardData {
 	//[1][2][3]
 	//[4][5][6]
 	//[7][8][9]
+	
+	public BoardData() {
+		super();
+	}
+
+	private char [] board={0,0,0,0,0,0,0,0,0};
+	
 	private int[][] combinations = {
 			{1,5,9},
 			{3,5,7},
@@ -15,53 +22,19 @@ public class BoardData {
 			{2,5,8},
 			{3,6,9}
 	};
-	private boolean isAvailable;
-	private int buttonNumber;
-	public BoardData(boolean isAvailable,int buttonNumber) {
-		this.isAvailable = isAvailable;
-		this.buttonNumber = buttonNumber;
-	}
 	
-	public boolean getIsAvailable() {
-		return isAvailable;
+	public char getField(int Field){
+		return this.board[Field];
 	}
-	
-	public void setIsAvailable(boolean isAvailable) {
-		this.isAvailable = isAvailable;
+	public void setField(int Field, char Symbol){
+		this.board[Field]=Symbol;
 	}
-	
-	public int getButtonNumber() {
-		return buttonNumber;
-	}
-	
-	public void setButtonNumber(int buttonNumber) {
-		this.buttonNumber = buttonNumber;
-	}
-	
-	public void makeUnavailable() {
-		if(!isAvailable) {
-			for(int raw = 0; raw < 8; raw++) {
-				for(int col = 0; col < 8; col++) {
-					if(combinations[raw][col] == buttonNumber) {
-						this.combinations[raw][col] = -1 * buttonNumber;
-					}
-				}
-			}
+	public void resetBoard(){
+		for(int i=0;i<9;i++){
+			this.board[i]=0;
 		}
+	}
+	public boolean isComplete(){
 		
 	}
-	
-	public void checkAvailable() {
-		for(int raw = 0; raw < 8; raw++) {
-			for(int col = 0; col < 8; col++) {
-				if(combinations[raw][col] > 0) {
-					this.isAvailable = true;
-				}else {
-					this.isAvailable = false;
-				}
-			}
-		}
-		
-	}
-
 }
