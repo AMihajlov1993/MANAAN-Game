b5176366284a802df7444a9627bb76d0bad96f5b
soxa <emil.aleksandrov87@gmail.com>
2014-05-21 15:16:50 +0300

    Commit Machite

--- src/TicTackPacage/Machine.java
+++ src/TicTackPacage/Machine.java
@@ -12,29 +12,26 @@ public class Machine {
 		int myPlayerTurn = 0;
 		boolean isPlaying = true;
 		boolean isWinner = false;
-		while(isPlaying) {
-			if(myPlayerTurn % 2 == 0) {
+		while (isPlaying) {
+			if (myPlayerTurn % 2 == 0) {
 				myPlayer.setSymbol(TicToeEnum.X);
 				currentPlayer = myPlayer;
-				
-			
-			}else {
+
+			} else {
 				bot.setSymbol(TicToeEnum.O);
 				currentPlayer = bot;
 			}
 			myPlayerTurn++;
-			
-			
-			
-			if(isWinner) {
+
+			if (isWinner) {
 				isPlaying = false;
 			}
-			
-			if(myPlayerTurn == 8) {
+
+			if (myPlayerTurn == 8) {
 				isPlaying = false;
 			}
 		}
-		
+
 	}
-	
+
 }
